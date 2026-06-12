b7332afe4659a99349cee5dfa2dee6afd50a87ef
ameliaandbanee <ameliaandbanee@gmail.com>
2026-06-12 16:13:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import asyncio
 import os
-from telegram import Bot
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_USERNAME = "@GoodnewsdailySriLanka"
-
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-
-    await bot.send_message(
-        chat_id=CHANNEL_USERNAME,
-        text="🙏 Test message from Good News Daily Bot\n\nRailway + GitHub + Telegram are connected successfully."
-    )
-
-    print("Message sent successfully!")
-
-asyncio.run(main())
+print("BOT_TOKEN =", repr(os.getenv("BOT_TOKEN")))
