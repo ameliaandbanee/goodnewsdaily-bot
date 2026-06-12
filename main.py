048b8d9a1291ef8ddfe48c95d1d9221fabe5423b
ameliaandbanee <ameliaandbanee@gmail.com>
2026-06-12 15:47:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Good News Daily Bot Started")
+import asyncio
+from telegram import Bot
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+CHANNEL_USERNAME = "@GoodnewsdailySriLanka"
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+
+    await bot.send_message(
+        chat_id=CHANNEL_USERNAME,
+        text="🙏 Test message from Good News Daily Bot"
+    )
+
+asyncio.run(main())
