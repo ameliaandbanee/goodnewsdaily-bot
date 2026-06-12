d812ab7495b96fb40cb58dc825eec3333b0e3cb8
ameliaandbanee <ameliaandbanee@gmail.com>
2026-06-12 15:54:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import asyncio
-from telegram import Bot
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+token = os.getenv("BOT_TOKEN")
 
-CHANNEL_USERNAME = "@GoodnewsdailySriLanka"
+print("TOKEN EXISTS:", token is not None)
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-
-    await bot.send_message(
-        chat_id=CHANNEL_USERNAME,
-        text="🙏 Test message from Good News Daily Bot"
-    )
-
-asyncio.run(main())
+if token:
+    print("TOKEN LENGTH:", len(token))
+else:
+    print("TOKEN NOT FOUND")
